64efbadfd7f1a86a9c385c4d8531ccdd439dda3b
test7102 <vivenandzuana@mail.ru>
2017-04-10 19:47:02 +0300

    Create Randomer.java
    10.04.17 Задание 9

---
+++ Randomer.java
@@ -0,0 +1,20 @@
+package com.company;
+/**
+ * Задание номер 9
+ */
+public class Randomer {
+
+    public static void main(String[] args) {
+        String sign="!";
+
+        System.out.println("Hello, world!\nAnd hi again!");
+        for (int i = 0; i < randomrange(); i++) {
+            System.out.print(sign);
+
+        }
+
+    }
+    public static int randomrange(){
+        return (int)(Math.random()*46)+5;
+    }
+}
