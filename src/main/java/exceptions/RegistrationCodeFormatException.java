ae3312fd462b7cb11a91f0dfec7b6d6ba23eb1ce
test7102 <vivenandzuana@mail.ru>
2017-05-19 00:48:06 +0300

    v1

--- src/main/java/exceptions/RegistrationCodeFormatException.java
+++ src/main/java/exceptions/RegistrationCodeFormatException.java
@@ -1,6 +1,6 @@
 package main.java.exceptions;
 
-public class RegistrationCodeFormatException extends NumberFormatException {
+public class RegistrationCodeFormatException extends IllegalArgumentException {
 	public RegistrationCodeFormatException(String message) {
 		super(message);
 	}
